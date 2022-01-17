2fc2346f6889bbf2319ce1ab88ea40673b5abbb0
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-16 18:14:15 -0800

    pwa

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
 module.exports = {
   devServer: {
     static: "./",
@@ -50,6 +52,24 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          //   sizes: [96, 128],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
   mode: "development",
